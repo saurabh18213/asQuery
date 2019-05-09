2b1a71b677aa1ae012e0b2ad15c64908ea2c8127
Saurabh Bansal <b17059@students.iitmandi.ac.in>
2019-05-09 22:55:25 +0530

    Login start

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from flask_mysqldb import MySQL
+from app.forms import LoginForm
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -23,3 +24,12 @@ def index():
 @app.route('/question')
 def question():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
