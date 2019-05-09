cff5f0b0803323f9abed522ac02795972d82e9c4
Saurabh Bansal <b17059@students.iitmandi.ac.in>
2019-05-09 16:42:20 +0530

    flask_mysql added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from app import app
+from flask_mysqldb import MySQL
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'cs309_project'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    cur = mysql.connection.cursor()
+    cur.execute("Select * from User")
+    rv = cur.fetchall()
+    print(rv)
     return render_template('base.html')
 
 @app.route('/question')
